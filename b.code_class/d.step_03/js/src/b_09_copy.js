8e121874679a3f475c8038588e86e9efeb3dcae4
hyunao <gusdk99323@gmail.com>
2021-11-16 00:03:40 +0900

    방과후 수업

--- b.code_class/d.step_03/js/src/b_09_copy.js
+++ b.code_class/d.step_03/js/src/b_09_copy.js
@@ -28,6 +28,12 @@ var cloneColor = [];
   console.log(favoritColor, cloneColor);
 */
 
+var copyColor=[];
+// for 반복문=깊은 복사
+for(var i=0; i<5;i++){
+  copyColor[i]=favoritColor[i];
+}
+
 var i = 0;
 var len = favoritColor.length;
 // 방법2 -------------------------
@@ -68,7 +74,8 @@ var pc = {
   'dell'    : '프리시전',
   'hp'      : 'z시리즈',
   'apple'   : 'mac book',
-  'samsung' : 'galaxy book'
+  'samsung' : 'galaxy book'//,
+  // 'lg' : 'gear'
 }; 
 var copyPc = pc;
 // pc.lg = 'gear';
@@ -92,6 +99,13 @@ for(var data in pc){
   // console.log( data );
   clonePc[data] = pc[data];
 }
+// for(in){} -> for in문
+/* 값 자체에 접근하기
+for(var key in pc(원본)){
+  clonePc[key]=pc[key];->clonePc의 key(변수명)와 pc의 key는 같다 // 깊은 복사
+}
+
+*/
 
 pc['asus'] = 'gen book';
 clonePc['lenovo'] = 'think pad';
@@ -103,8 +117,39 @@ console.log(clonePc);
 // 참조 주소에 있는 값 자체를 복사 - 깊은 복사(deep copy)
 
 // --------------------------------------------------------
-// var cookie = ['초코칩','칙촉','빼빼로','호빵','촉촉한초코칩','칸쵸','홈런볼','엄마손'];
-// var snack = { '농심':'새우깡', '해태':'맛동산', '오리온':'고래밥', '크라운':'산도' };
+// 깊은 복사를 한 뒤 원하는 제품 하나를 추가하세요
+var cookie = ['초코칩','칙촉','빼빼로','호빵','촉촉한초코칩','칸쵸','홈런볼','엄마손'];
+var cloneCookie=[];
+
+// 깊은복사
+for (var i = 0; i<cookie.length;){
+cloneCookie.push(cookie[i]);
+}
+console.log(cookie === cloneCookie);
+cloneCookie.push('고래밥');
+console.log(cookie, cloneCookie);
+// [ '초코칩', '칙촉', '빼빼로', '호빵', '촉촉한초코칩', '칸초', '홈런볼', '엄마손' ]
+// [ '초코칩', '칙촉', '빼빼로', '호빵', '촉촉한초코칩', '칸초', '홈런볼', '엄마손', '고래밥' ]
+
+// =======================================================================================
+
+var snack = { '농심':'새우깡', '해태':'맛동산', '오리온':'고래밥', '크라운':'산도' };
+var cloneSnack={};
+
+// 깊은 복사
+for (var key in snack) {
+  cloneSnack[key]=snack[key];
+  // 들어갈 아이=들어올 곳
+}
+
+// 제품 추가
+snack.롯데 = '빈츠';
+// snack.롯데(식별자);
+cloneSnack['삼우식품']='찰떡파이';
+
+// { '농심': '새우깡', '해태': '맛동산', '오리온': '고래밥', '크라운': '산도' } 
+// { '농심': '새우깡', '해태': '맛동산', '오리온': '고래밥', '크라운': '산도', '롯데': '마가렛트' }
+
 // var ice = [ {'롯데':['폴라포','수박바']},{'해태':'브라보'},{'허쉬':'민트초코'},{'빙그레':'투게더'} ];
 
 // 객체도 참조변수
