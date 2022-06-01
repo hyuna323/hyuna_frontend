c9d4ba7fb3d1f4b29292daa18faacfdd5897c3d5
hyunao <gusdk99323@gmail.com>
2022-06-01 13:00:42 +0900

    공지사항 생성

--- A.portfolio/js/src/01_ediya_main.js
+++ A.portfolio/js/src/01_ediya_main.js
@@ -39,3 +39,40 @@ function showSlides(n) {
   slides[n].style.display = "block";
   dots[n].className += " active";
 }
+
+// ---------------------------------------------
+
+// mainpage
+function home(){
+  let url = "http://127.0.0.1:5500/html/01_ediya_main.html";
+  window.location = url;
+}
+
+// SNS Facebook
+function sns_F(){
+  let url = "https://www.facebook.com/iEDIYA";
+  window.location = url;
+}
+
+// SNS Instargram
+function sns_I(){
+  let url = "https://www.instagram.com/ediya.coffee/";
+  window.location = url;
+}
+
+// SNS Naver blog
+function sns_I(){
+  let url = "https://blog.naver.com/ediya-coffee";
+  window.location = url;
+}
+
+// --------------------------
+// 이디야 소식 버튼
+
+//공지사항 버튼 클릭
+$('.notice_on').click(function(){
+  $('.notice_letter').show(); //공지사항 내용 나타내기
+  $('.gallery_img').hide(); //갤러리 내용 숨기기
+  $(this).attr('class','notice_on'); //공지사항 버튼 CSS변경
+  $('.gallery_on').attr('class','gallery_off'); //갤러리 버튼 CSS 변경
+});
