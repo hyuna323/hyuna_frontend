8b07fadb1956356f6d5beafd8bbdf563045ec62a
hyunao <89890370+hyunao@users.noreply.github.com>
2022-10-05 06:57:02 +0900

    login page 완성

---
+++ A.portfolio/protfolio_react_ex/js/src/01_ediya_login.js
@@ -0,0 +1,9 @@
+function home(){
+  let url = "http://127.0.0.1:5502/html/01_ediya_main.html";
+  window.location = url;
+}
+
+function login(){
+let url = "http://127.0.0.1:5502/html/02_ediya_login.html";
+window.location = url;
+}
