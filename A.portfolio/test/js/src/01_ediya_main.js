258a01ebd58ece916770e791334b772f585d435c
hyunao <89890370+hyunao@users.noreply.github.com>
2022-10-11 09:59:32 +0900

    햄버거 메뉴 html, css 완성
    헷갈리지 않게 test 폴더를 따로 생성해서 햄버거 메뉴를 만들어 봄
    js까지 완성 시 원래 포트폴리오 폴더에 붙일 예정

---
+++ A.portfolio/test/js/src/01_ediya_main.js
@@ -0,0 +1,113 @@
+var slideIndex = 0;
+
+// html 끝난 후 동작
+window.onload=function(){
+  showSlides(slideIndex);
+
+  const sec = 3000;
+  setInterval(function(){
+    slideIndex++;
+    showSlides(slideIndex);
+
+  }, sec);
+}
+function currentSlide(n) {
+  slideIndex = n;
+  showSlides(slideIndex);
+}
+
+function showSlides(n) {
+
+  const slides = document.getElementsByClassName("viewport");
+  const dots = document.getElementsByClassName("dot");
+  const size = slides.length;
+
+  if ((n+1) > size) {
+    slideIndex = 0; n = 0;
+  }else if (n < 0) {
+    slideIndex = (size-1);
+    n = (size-1);
+  }
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[n].style.display = "block";
+  dots[n].className += " active";
+}
+
+// header fixed
+// let header = document.querySelector(".header");
+// let headerHeight = header.offsetHeight;
+
+// window.onscroll = function () {
+//   let windowTop = window.scrollY;
+//   if (windowTop >= headerHeight) {
+//     header.classList.add("drop");
+//   } else {
+//     header.classList.remove("drop");
+//   }
+// };
+
+// ---------------------------------------------
+
+// mainpage
+function home(){
+  // 베르첼 배포시 url 바꾸기
+  let url = "http://127.0.0.1:5500/html/01_ediya_main.html";
+  window.location = url;
+}
+
+// loginpage
+function login(){
+  let url = "http://127.0.0.1:5502/html/02_ediya_login.html";
+  window.location = url;
+  }
+
+// SNS Facebook
+function sns_F(){
+  let url = "https://www.facebook.com/iEDIYA";
+  window.location = url;
+}
+
+// SNS Instargram
+function sns_I(){
+  let url = "https://www.instagram.com/ediya.coffee/";
+  window.location = url;
+}
+
+// SNS Naver blog
+function sns_N(){
+  let url = "https://blog.naver.com/ediya-coffee";
+  window.location = url;
+}
+
+// --------------------------
+// 이디야 소식 버튼
+
+//공지사항 버튼 클릭
+$('.notice_on').click(function(){
+  $('.notice_letter').show(); //공지사항 내용 나타내기
+  $('.gallery_img').hide(); //갤러리 내용 숨기기
+  $(this).attr('class','notice_on'); //공지사항 버튼 CSS변경
+  $('.gallery_on').attr('class','gallery_off'); //갤러리 버튼 CSS 변경
+});
+
+// // 메뉴네비
+// // 변수영역 ----------------------------------
+// var navArea = document.querySelector('.nav_area');
+// var naviBtnSelector = document.querySelector('.navi_btn');
+// var naviBtn = naviBtnSelector.querySelector('button');
+// var navOn = 'on';
+
+// // 이벤트영역 ----------------------------------
+// naviBtn.addEventListener('click', function(event){
+//   event.preventDefault();
+//   // navArea.classList.toggle('on');  
+//   var checkClassName = navArea.classList.contains(navOn);
+//   (checkClassName) ? navArea.classList.remove(navOn) : navArea.classList.add(navOn);
+// });
---
+++ A.portfolio/test/js/src/02_ediya_login.js
@@ -0,0 +1,27 @@
+function home(){
+  let url = "http://127.0.0.1:5502/html/01_ediya_main.html";
+  window.location = url;
+}
+
+function login(){
+let url = "http://127.0.0.1:5502/html/02_ediya_login.html";
+window.location = url;
+}
+
+// SNS Facebook
+function sns_F(){
+  let url = "https://www.facebook.com/iEDIYA";
+  window.location = url;
+}
+
+// SNS Instargram
+function sns_I(){
+  let url = "https://www.instagram.com/ediya.coffee/";
+  window.location = url;
+}
+
+// SNS Naver blog
+function sns_N(){
+  let url = "https://blog.naver.com/ediya-coffee";
+  window.location = url;
+}
