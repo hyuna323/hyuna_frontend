4ec8e51e0402e16fabc370b98ccaf9f13394e99a
hyunao <89890370+hyunao@users.noreply.github.com>
2022-10-15 05:36:49 +0900

    react setting 및 변수 적용

--- b.code_class/b_step_07/src/index.js
+++ b.code_class/b_step_07/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; //./App아라는 아이를 App이라고 부르겠다는 소리
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render( 
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>, 
+  document.getElementById('root')
 );
+// render라는 함수는 React.StrictMode라는 영역 안에서 App이라는 기능을 수행하겠다는 의미
+// ReactDOM라는 기능을 통해서 render라는 함수를 수행하겠다는 의미
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
