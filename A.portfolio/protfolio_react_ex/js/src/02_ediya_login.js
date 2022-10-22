d0a71e145f0edf898298ac7c191ef50174309878
hyunao <89890370+hyunao@users.noreply.github.com>
2022-10-23 07:26:46 +0900

    ediya login page modal & checkbox, sidemenu 수정

--- A.portfolio/protfolio_react_ex/js/src/02_ediya_login.js
+++ A.portfolio/protfolio_react_ex/js/src/02_ediya_login.js
@@ -25,3 +25,20 @@ function sns_N(){
   let url = "https://blog.naver.com/ediya-coffee";
   window.location = url;
 }
+
+
+// --------------------------------
+// modal
+const openButton = document.getElementById("open");
+const modal = document.querySelector(".modal");
+const overlay = modal.querySelector(".modal_overlay");
+const closeBtn = modal.querySelector("button");
+const openModal = () => {
+  modal.classList.remove("on");
+}
+const colseModal = () => {
+  modal.classList.add("on");
+}
+overlay.addEventListener("click", colseModal);
+closeBtn.addEventListener("click", colseModal);
+openButton.addEventListener("click", openModal);
