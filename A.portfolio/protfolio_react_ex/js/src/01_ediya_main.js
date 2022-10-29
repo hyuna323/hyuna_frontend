a6470d12cd4f80da044d8ffa03754bbd608feac7
hyunao <89890370+hyunao@users.noreply.github.com>
2022-10-30 08:37:05 +0900

    tab..

--- A.portfolio/protfolio_react_ex/js/src/01_ediya_main.js
+++ A.portfolio/protfolio_react_ex/js/src/01_ediya_main.js
@@ -1,47 +1,165 @@
+// 01_ediya_main.js
+
 // navi menu
-// 변수영역 ----------------------------------
-var gnb = document.querySelector('.gnb');
-var subDropSelector = document.querySelector('.sub_drop');
-var subDrop = subDropSelector.querySelector('button');
-var navOn = 'open';
+// // menu ----------------------------------------------------
+// var menuPartSelector = document.querySelector('.menu_part');
+// var menuPart = menuPartSelector.querySelector('button');
+// var menuInner = document.querySelector('.menu_inner');
 
-// 이벤트영역 ----------------------------------
-subDrop.addEventListener('click', function(event){
-  event.preventDefault();
-  // navArea.classList.toggle('on');  
-  var checkClassName = gnb.classList.contains(navOn);
-  (checkClassName) ? gnb.classList.remove(navOn) : gnb.classList.add(navOn);
-});
-
-// 변수 ----------------------------------------------------
-var menuDropSelector = document.querySelector('.sub_drop');
-var subDrop = subDropSelector.querySelector('button');
-var subDropInner = document.querySelector('.sub_drop_inner');
-
-var subDropInnerStyle = getComputedStyle(subDropInner);
+// var menuInnerStyle = getComputedStyle(menuInner);
+// // console.log( menuInnerStyle.display );
 
 
-var displayStateFn = function(selector){
-  // 선택자의 display 상태값을 가져오는데 기본 html문서에서 값을 가져오되 없다면, css기본값에서 가져오게
-    return selector.style.display || getComputedStyle(selector).display;
-};
+// var displayStateFn = function(selector){
+//   // 선택자의 display 상태값을 가져오는데 기본 html문서에서 값을 가져오되 없다면, css기본값에서 가져오게
+//     return selector.style.display || getComputedStyle(selector).display;
+//     /*
+//     var displayState;
+//       if(!!selector.style.display){
+//         displayState = selector.style.display
+//       }else {
+//         displayState = getComputedStyle(selector).display;
+//       }
+//     */
+// };
 
-// 이벤트 ----------------------------------------------------
-subDrop.addEventListener('click', function(event){
-  event.preventDefault();
-  var state = displayStateFn(subDropInner);
-  console.log( state );
-  (state === 'none') ? subDropInner.style.display = 'block' :  subDropInner.style.display = 'none';
-});
-// ----------------------------------------------------------------
-console.clear();
+// // 이벤트
+// menuPart.addEventListener('click', function(event){
+//   event.preventDefault();
+//   // (menuInnerStyle.display === 'none') ? menuInner.style.display = 'block' :  menuInner.style.display = 'none';
+//   var state = displayStateFn(menuInner);
+//   console.log( state );
+//   (state === 'none') ? menuInner.style.display = 'block' :  menuInner.style.display = 'none';
+// });
 
-var ck = subDropInner.style.display; // 0 , false, null, undefined, ''
-console.log( ck  );
-console.log( !ck  ); // 원값의 결과를 boolean타입의 형태로 강제로 치환하기 위한 부정으로 처리
-console.log( !!ck  ); // 원값의 처리가 boolean타입의 형태로 전환해보기 ( !! 두개 )
+// console.clear();
+
+// var ck = menuInner.style.display; // 0 , false, null, undefined, ''
+// console.log( ck  );
+// console.log( !ck  ); // 원값의 결과를 boolean타입의 형태로 강제로 치환하기 위한 부정으로 처리
+// console.log( !!ck  ); // 원값의 처리가 boolean타입의 형태로 전환해보기 ( !! 두개 )
 
 
+
+// // members ----------------------------------------------------
+// var membersPartSelector = document.querySelector('.members_part');
+// var membersPart = membersPartSelector.querySelector('button');
+// var membersInner = document.querySelector('.members_inner');
+
+// var membersInnerStyle = getComputedStyle(membersInner);
+// // console.log( membersInnerStyle.display );
+
+
+// var displayStateFn = function(selector){
+//   // 선택자의 display 상태값을 가져오는데 기본 html문서에서 값을 가져오되 없다면, css기본값에서 가져오게
+//     return selector.style.display || getComputedStyle(selector).display;
+//     /*
+//     var displayState;
+//       if(!!selector.style.display){
+//         displayState = selector.style.display
+//       }else {
+//         displayState = getComputedStyle(selector).display;
+//       }
+//     */
+// };
+
+// // 이벤트
+// membersPart.addEventListener('click', function(event){
+//   event.preventDefault();
+//   // (membersInnerStyle.display === 'none') ? membersInner.style.display = 'block' :  membersInner.style.display = 'none';
+//   var state = displayStateFn(membersInner);
+//   console.log( state );
+//   (state === 'none') ? membersInner.style.display = 'block' :  membersInner.style.display = 'none';
+// });
+
+// console.clear();
+
+// var ck = membersInner.style.display; // 0 , false, null, undefined, ''
+// console.log( ck  );
+// console.log( !ck  ); // 원값의 결과를 boolean타입의 형태로 강제로 치환하기 위한 부정으로 처리
+// console.log( !!ck  ); // 원값의 처리가 boolean타입의 형태로 전환해보기 ( !! 두개 )
+
+
+// // gift ----------------------------------------------------
+// var giftPartSelector = document.querySelector('.gift_part');
+// var giftPart = giftPartSelector.querySelector('button');
+// var giftInner = document.querySelector('.gift_inner');
+
+// var giftInnerStyle = getComputedStyle(giftInner);
+// // console.log( giftInnerStyle.display );
+
+
+// var displayStateFn = function(selector){
+//   // 선택자의 display 상태값을 가져오는데 기본 html문서에서 값을 가져오되 없다면, css기본값에서 가져오게
+//     return selector.style.display || getComputedStyle(selector).display;
+//     /*
+//     var displayState;
+//       if(!!selector.style.display){
+//         displayState = selector.style.display
+//       }else {
+//         displayState = getComputedStyle(selector).display;
+//       }
+//     */
+// };
+
+// // 이벤트
+// giftPart.addEventListener('click', function(event){
+//   event.preventDefault();
+//   // (giftInnerStyle.display === 'none') ? giftInner.style.display = 'block' :  giftInner.style.display = 'none';
+//   var state = displayStateFn(giftInner);
+//   console.log( state );
+//   (state === 'none') ? giftInner.style.display = 'block' :  giftInner.style.display = 'none';
+// });
+
+// console.clear();
+
+// var ck = giftInner.style.display; // 0 , false, null, undefined, ''
+// console.log( ck  );
+// console.log( !ck  ); // 원값의 결과를 boolean타입의 형태로 강제로 치환하기 위한 부정으로 처리
+// console.log( !!ck  ); // 원값의 처리가 boolean타입의 형태로 전환해보기 ( !! 두개 )
+
+
+// // announcement -----------------------------------------------
+// var announcementPartSelector = document.querySelector('.announcement_part');
+// var announcementPart = announcementPartSelector.querySelector('button');
+// var announcementInner = document.querySelector('.announcement_inner');
+
+// var announcementInnerStyle = getComputedStyle(announcementInner);
+// // console.log( announcementInnerStyle.display );
+
+
+// var displayStateFn = function(selector){
+//   // 선택자의 display 상태값을 가져오는데 기본 html문서에서 값을 가져오되 없다면, css기본값에서 가져오게
+//     return selector.style.display || getComputedStyle(selector).display;
+//     /*
+//     var displayState;
+//       if(!!selector.style.display){
+//         displayState = selector.style.display
+//       }else {
+//         displayState = getComputedStyle(selector).display;
+//       }
+//     */
+// };
+
+// // 이벤트
+// announcementPart.addEventListener('click', function(event){
+//   event.preventDefault();
+//   // (announcementInnerStyle.display === 'none') ? announcementInner.style.display = 'block' :  announcementInner.style.display = 'none';
+//   var state = displayStateFn(announcementInner);
+//   console.log( state );
+//   (state === 'none') ? announcementInner.style.display = 'block' :  announcementInner.style.display = 'none';
+// });
+
+// console.clear();
+
+// var ck = announcementInner.style.display; // 0 , false, null, undefined, ''
+// console.log( ck  );
+// console.log( !ck  ); // 원값의 결과를 boolean타입의 형태로 강제로 치환하기 위한 부정으로 처리
+// console.log( !!ck  ); // 원값의 처리가 boolean타입의 형태로 전환해보기 ( !! 두개 )
+
+
+
+// viewBox ==============================
 var slideIndex = 0;
 
 // html 끝난 후 동작
--- A.portfolio/test/js/src/01_ediya_main.js
+++ A.portfolio/test/js/src/01_ediya_main.js
@@ -1,29 +1,13 @@
-// 변수영역 ----------------------------------
-var gnb = document.querySelector('.gnb');
-var naviInnerSelector = document.querySelector('.navi_inner');
-var naviInner = naviInnerSelector.querySelector('button');
-var navOn = 'on';
+// 01_ediya_main.js
 
-// 이벤트영역 ----------------------------------
-naviInner.addEventListener('click', function(event){
-  event.preventDefault();
-  // gnb.classList.toggle('on');  
-  var checkClassName = gnb.classList.contains(navOn);
-  // (checkClassName) ? gnb.classList.remove(navOn) : gnb.classList.add(navOn);
-  if(checkClassName){
-    gnb.classList.remove(navOn);
-  }else{
-    gnb.classList.add(navOn);
-  }
-});
 
-// 변수 ----------------------------------------------------
-var dropBtnSelector = document.querySelector('.drop_btn');
-var dropBtn = dropBtnSelector.querySelector('button');
-var subMenu = document.querySelector('.sub_menu');
+// menu ----------------------------------------------------
+var menuPartSelector = document.querySelector('.menu_part');
+var menuPart = menuPartSelector.querySelector('button');
+var menuInner = document.querySelector('.menu_inner');
 
-var subMenuStyle = getComputedStyle(subMenu);
-// console.log( subMenuStyle.display );
+var menuInnerStyle = getComputedStyle(menuInner);
+// console.log( menuInnerStyle.display );
 
 
 var displayStateFn = function(selector){
@@ -40,17 +24,135 @@ var displayStateFn = function(selector){
 };
 
 // 이벤트 ----------------------------------------------------
-dropBtn.addEventListener('click', function(event){
+menuPart.addEventListener('click', function(event){
   event.preventDefault();
-  // (subMenuStyle.display === 'none') ? subMenu.style.display = 'block' :  subMenu.style.display = 'none';
-  var state = displayStateFn(subMenu);
+  // (menuInnerStyle.display === 'none') ? menuInner.style.display = 'block' :  menuInner.style.display = 'none';
+  var state = displayStateFn(menuInner);
   console.log( state );
-  (state === 'none') ? subMenu.style.display = 'block' :  subMenu.style.display = 'none';
+  (state === 'none') ? menuInner.style.display = 'block' :  menuInner.style.display = 'none';
 });
 // ----------------------------------------------------------------
 console.clear();
 
-var ck = subMenu.style.display; // 0 , false, null, undefined, ''
+var ck = menuInner.style.display; // 0 , false, null, undefined, ''
+console.log( ck  );
+console.log( !ck  ); // 원값의 결과를 boolean타입의 형태로 강제로 치환하기 위한 부정으로 처리
+console.log( !!ck  ); // 원값의 처리가 boolean타입의 형태로 전환해보기 ( !! 두개 )
+
+
+
+// members ----------------------------------------------------
+var membersPartSelector = document.querySelector('.members_part');
+var membersPart = membersPartSelector.querySelector('button');
+var membersInner = document.querySelector('.members_inner');
+
+var membersInnerStyle = getComputedStyle(membersInner);
+// console.log( membersInnerStyle.display );
+
+
+var displayStateFn = function(selector){
+  // 선택자의 display 상태값을 가져오는데 기본 html문서에서 값을 가져오되 없다면, css기본값에서 가져오게
+    return selector.style.display || getComputedStyle(selector).display;
+    /*
+    var displayState;
+      if(!!selector.style.display){
+        displayState = selector.style.display
+      }else {
+        displayState = getComputedStyle(selector).display;
+      }
+    */
+};
+
+// 이벤트 ----------------------------------------------------
+membersPart.addEventListener('click', function(event){
+  event.preventDefault();
+  // (membersInnerStyle.display === 'none') ? membersInner.style.display = 'block' :  membersInner.style.display = 'none';
+  var state = displayStateFn(membersInner);
+  console.log( state );
+  (state === 'none') ? membersInner.style.display = 'block' :  membersInner.style.display = 'none';
+});
+// ----------------------------------------------------------------
+console.clear();
+
+var ck = membersInner.style.display; // 0 , false, null, undefined, ''
+console.log( ck  );
+console.log( !ck  ); // 원값의 결과를 boolean타입의 형태로 강제로 치환하기 위한 부정으로 처리
+console.log( !!ck  ); // 원값의 처리가 boolean타입의 형태로 전환해보기 ( !! 두개 )
+
+
+// gift ----------------------------------------------------
+var giftPartSelector = document.querySelector('.gift_part');
+var giftPart = giftPartSelector.querySelector('button');
+var giftInner = document.querySelector('.gift_inner');
+
+var giftInnerStyle = getComputedStyle(giftInner);
+// console.log( giftInnerStyle.display );
+
+
+var displayStateFn = function(selector){
+  // 선택자의 display 상태값을 가져오는데 기본 html문서에서 값을 가져오되 없다면, css기본값에서 가져오게
+    return selector.style.display || getComputedStyle(selector).display;
+    /*
+    var displayState;
+      if(!!selector.style.display){
+        displayState = selector.style.display
+      }else {
+        displayState = getComputedStyle(selector).display;
+      }
+    */
+};
+
+// 이벤트 ----------------------------------------------------
+giftPart.addEventListener('click', function(event){
+  event.preventDefault();
+  // (giftInnerStyle.display === 'none') ? giftInner.style.display = 'block' :  giftInner.style.display = 'none';
+  var state = displayStateFn(giftInner);
+  console.log( state );
+  (state === 'none') ? giftInner.style.display = 'block' :  giftInner.style.display = 'none';
+});
+// ----------------------------------------------------------------
+console.clear();
+
+var ck = giftInner.style.display; // 0 , false, null, undefined, ''
+console.log( ck  );
+console.log( !ck  ); // 원값의 결과를 boolean타입의 형태로 강제로 치환하기 위한 부정으로 처리
+console.log( !!ck  ); // 원값의 처리가 boolean타입의 형태로 전환해보기 ( !! 두개 )
+
+
+// announcement -----------------------------------------------
+var announcementPartSelector = document.querySelector('.announcement_part');
+var announcementPart = announcementPartSelector.querySelector('button');
+var announcementInner = document.querySelector('.announcement_inner');
+
+var announcementInnerStyle = getComputedStyle(announcementInner);
+// console.log( announcementInnerStyle.display );
+
+
+var displayStateFn = function(selector){
+  // 선택자의 display 상태값을 가져오는데 기본 html문서에서 값을 가져오되 없다면, css기본값에서 가져오게
+    return selector.style.display || getComputedStyle(selector).display;
+    /*
+    var displayState;
+      if(!!selector.style.display){
+        displayState = selector.style.display
+      }else {
+        displayState = getComputedStyle(selector).display;
+      }
+    */
+};
+
+// 이벤트 ----------------------------------------------------
+announcementPart.addEventListener('click', function(event){
+  event.preventDefault();
+  // (announcementInnerStyle.display === 'none') ? announcementInner.style.display = 'block' :  announcementInner.style.display = 'none';
+  var state = displayStateFn(announcementInner);
+  console.log( state );
+  (state === 'none') ? announcementInner.style.display = 'block' :  announcementInner.style.display = 'none';
+});
+// ----------------------------------------------------------------
+console.clear();
+
+var ck = announcementInner.style.display; // 0 , false, null, undefined, ''
 console.log( ck  );
 console.log( !ck  ); // 원값의 결과를 boolean타입의 형태로 강제로 치환하기 위한 부정으로 처리
 console.log( !!ck  ); // 원값의 처리가 boolean타입의 형태로 전환해보기 ( !! 두개 )
