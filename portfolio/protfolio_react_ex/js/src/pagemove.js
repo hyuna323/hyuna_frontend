992ae098a3a7590a6e5ee5be24a41e5395ad7d20
hyunao <89890370+hyunao@users.noreply.github.com>
2022-12-04 01:29:00 +0900

    view box 이미지 수정, font-size 수정

--- portfolio/protfolio_react_ex/js/src/pagemove.js
+++ portfolio/protfolio_react_ex/js/src/pagemove.js
@@ -2,19 +2,19 @@
 // mainpage
 // 베르첼 배포시 url 바꾸기
 function home(){
-  let url = "http://127.0.0.1:5502/html/01_ediya_main.html";
+  let url = "https://ediyaproject.vercel.app/html/01_ediya_main.html";
   window.location = url;
 }
 
 // loginpage
 function login(){
-  let url = "http://127.0.0.1:5502/html/02_ediya_login.html";
+  let url = "https://ediyaproject.vercel.app/html/03_ediya_login.html";
   window.location = url;
   }
 
 // aboutpage
 function about(){
-  let url = "http://127.0.0.1:5502/html/03_ediya_about.html";
+  let url = "https://ediyaproject.vercel.app/html/03_ediya_about.html";
   window.location = url;
   }
 
