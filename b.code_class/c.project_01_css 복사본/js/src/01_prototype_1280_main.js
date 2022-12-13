82d33f827538f1fdf49263641af3ce7018ca43bc
hyunao <89890370+hyunao@users.noreply.github.com>
2022-12-14 03:56:23 +0900

    <오늘의집> 1280px 수정

--- b.code_class/c.project_01_css 복사본/js/src/01_prototype_1280_main.js
+++ b.code_class/c.project_01_css 복사본/js/src/01_prototype_1280_main.js
@@ -52,3 +52,21 @@ function showSlides(n) {
   slides[n].style.display = "block";
   dots[n].className += " active";
 }
+
+// 관련 선택
+//footsubmenu 선택, language part 선택, language_btn선택 ul 선택
+const elfootSubmenu = document.querySelector('.footsubmenu');
+const elBrandsitePart = elfootSubmenu.querySelector('.brandsite_part');
+const BrandsiteBtn = elBrandsitePart.querySelector('.brandsite_btn');
+const Belul = elBrandsitePart.querySelector('ul');
+
+BrandsiteBtn.addEventListener('click', e => {
+  e.preventDefault();
+  let check = Belul.classList.contains('on');
+  (check) ? Belul.classList.remove('on') : Belul.classList.add('on');
+});
+
+elBrandsitePart.addEventListener('mouseleave', e => {
+  e.preventDefault();
+  Belul.classList.remove('on');
+});
