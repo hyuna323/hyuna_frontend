3e2e1a4c712a38b96390faf67a09bcf246f58916
hyunao <89890370+hyuna323@users.noreply.github.com>
2023-02-25 07:10:18 +0900

    todo list

--- e.nomadcoder/js/todo.js
+++ e.nomadcoder/js/todo.js
@@ -7,12 +7,16 @@ const toDoInput = toDoForm.querySelector("input");
 
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
 // 1. todo 저장하기
 // newTodo 그려질때마다 텍스트를 array에 push하기
-const toDos = [];
+// const가 아닌 let으로 바꿔 업데이트가 가능하도록 만들음
+// 항상 빈 array로 시작
+let toDos = [];
 
 function saveToDos() {
-  localStorage.setItem("todos", JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
   // JSON,stringify는 javascript object나 array 또는 어떤 javascript 코드건 간에 string으로 만들어줌
 }
 
@@ -22,6 +26,10 @@ function deleteToDo(event){
   
   // 버튼 클릭시 지워진다
   li.remove();
+
+  // 클릭한 li.id와 다른 toDo는 남겨두고 싶다는 의미
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 // localStorage
@@ -33,13 +41,14 @@ function paintToDo(newTodo){
   // li를 만듦
   // 변수 이름을 li라고 하지 않고 tomato라 고 적어도 됨 => html 태그의 이름으로 적을 필요는 없음
   const li = document.createElement("li");
+  li.id = newTodo.id;
   
   // span을 만듦
   // span이 li 안에 있지 않음
   const span = document.createElement("span");
 
   // span 안에 넣은 새로운 텍스트는 사용자가 form에서 우리에게 준 newTodo 값임
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   
   // append 하기 전에 삭제하는 button 만들기
   // click 이벤트가 필요함
@@ -69,11 +78,52 @@ function handleToDoSubmit(event) {
   // enter 누를 시 input이 비워짐
   // ""(빈칸)으로 저장하여 결과화면에 띄어쓰기가 있음
   toDoInput.value = "";
+
+  // 데이터베이스에게 id를 저장하는 옵션 주기 => 삭제가능하다는 얘기
+  const newTodoObj = {
+    text:newTodo,
+    
+    // Date.now() = 숫자를 랜덤으로 가져옴
+    // id가 있는 이유 : id로 각각의 title을 구별하고자 함
+    id: Date.now(),
+  };
   // newTodo를 toDos array에 push 하기
-  toDos.push(newTodo);
+  toDos.push(newTodoObj);
 
   paintToDo(newTodo);
   saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+// loading
+const savedToDos = localStorage.getItem(TODOS_KEY);
+// console.log(savedToDos);
+// localStorage에서 가져온 string을 사용 가능한 Javascript object로 만들기 -> JSON.parse(savedToDos)로 => array로 반환됨
+if(savedToDos !== null){
+  const parsedToDos = JSON.parse(savedToDos);
+  // localStroage에서 발견되는 이전의 todo들로 하고 싶음
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
+// 삭제를 하기 위해선 paintToDo에서 어떤 일이 일어나는 지 이해해야 함
+
+// filter 함수 : 지우고 싶은 item을 제외하고 새 array를 만든다
+
+
+// delete시 local storage에 업데이트 하는 방법
+// toDos array가 local storage와 같지 않음
+// todo에 id 주기
+
+
+
+// 첫번째 방법은 arrow function을 사용하는 방법 : (item) => console.log("this is the turn of", item)
+// 두번째 방법은 sayHello function을 만드는 방법 : function sayHello(item){console.log("this is the turn of", item)}
+
+// 이 방법처럼 해도 됨
+// if(saveToDos){
+//   const parsedToDos = JSON.parse(savedToDos);
+// }
+
+// console.log(savedToDos);
+// null이 나옴 왜냐면 localStorage에 아무것도 없기 때문
